33dc8f9267ee8e2a83b42be69e5ac32d2560235c
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2023-09-24 15:19:42 +0200

    screenshots_to_document.py: Add Table of Contents

--- screenshots_to_document.py
+++ screenshots_to_document.py
@@ -7,6 +7,40 @@ from docx.shared import Inches, Mm
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.oxml.xmlchemy import OxmlElement
 from docx.oxml.shared import qn
+import lxml.etree
+
+
+def add_toc(doc):
+    toc_heading = doc.add_heading("Table Of Contents", level=0)
+    toc_heading.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+    paragraph = doc.add_paragraph()
+    run = paragraph.add_run()
+
+    fldChar = OxmlElement("w:fldChar")
+    fldChar.set(qn("w:fldCharType"), "begin")
+
+    instrText = OxmlElement("w:instrText")
+    instrText.set(qn("xml:space"), "preserve")
+    instrText.text = 'TOC \\f \\o "1-9" \\h'
+
+    fldChar2 = OxmlElement("w:fldChar")
+    fldChar2.set(qn("w:fldCharType"), "separate")
+
+    fldChar3 = OxmlElement("w:t")
+    fldChar3.text = "Right-click to update field."
+
+    fldChar4 = OxmlElement("w:fldChar")
+    fldChar4.set(qn("w:fldCharType"), "end")
+
+    r_element = run._r
+    r_element.append(fldChar)
+    r_element.append(instrText)
+    r_element.append(fldChar2)
+    r_element.append(fldChar3)
+    r_element.append(fldChar4)
+
+    doc.add_paragraph()
 
 
 def add_borders(doc):
@@ -48,6 +82,7 @@ def create_document():
     doc.add_page_break()
 
     # Add empty page for table of contents
+    add_toc(doc)
     doc.add_page_break()
 
     return doc
